81bbd4eda30e3d9d82c22b4fed81d29aface983e
MontenegroMarcos <marcosmontenegro434@gmail.com>
2022-08-23 19:08:50 -0300

    Se anaiade Controlador

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -2,13 +2,24 @@ package com.example.demo;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
-    @FXML
+    /*@FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("se anadio restaurante!");
+    }*/
+
+    @FXML
+    private TextField campoNombre;
+
+    @FXML
+    protected void aniadirButtonClick(){
+        String nombre = campoNombre.getText();
+        System.out.println(nombre);
+        //return nombre;
     }
 }
